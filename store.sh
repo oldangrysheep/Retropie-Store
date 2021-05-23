27e9f3e67e941a705fd07a27ec4ba162e986e82e
oldangrysheep <50684907+oldangrysheep@users.noreply.github.com>
2021-05-22 22:12:46 -0400

    Update store.sh

--- store.sh
+++ store.sh
@@ -300,7 +300,131 @@ fi
 }
   
 Download_Specific_Games () {
-    echo "Specific Games"
+   ADVSEL=$(whiptail --title "Select The System You Want To Select The Games From" --menu "Choose an option" 15 60 4 \
+        "1" "Download All Selected" \
+        "2" "Download Selected In Backround" \
+        "3" "Nintendo Entertainment System" \
+        "4" "Super Nintendo Entertainemnt System" \
+        "5" "Gameboy" \
+        "6" "Gameboy Color" \
+        "7" "Gameboy Advanced" \
+        "8" "Nintendo 64" \
+        "9" "Nintendo Ds" \
+        "10" "Nintendo Virtual Boy" \
+        "11" "Megadrive Games" \
+        "12" "Sega Saturn" \
+        "13" "Sega Dreamcast" \
+        "14" "Playstation 1" \
+        "15" "Playstation Portable" 3>&1 1>&2 2>&3)
+    case $ADVSEL in
+        1)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/Download%20The%20Items%20You%20Selected.sh
+            sudo chmod +x "Download The Items You Selected.sh"
+            sudo sh "Download The Items You Selected.sh"
+            whiptail --title "Downloads Complete" --msgbox "Click Exit On The Main Menu Then Restart Emulation Station In Order To See Roms" 8 78
+            sudo sh downloader.sh
+        ;;
+        2)
+            whiptail --title "Downloads Started" --msgbox "You will now be redirected towards the start menu. To check on the download, select Downloads on the main menu. Dont start this command more than once." 8 78
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/Download%20In%20Backround.sh
+            sudo chmod +x "Download In Backround.sh"
+            exit
+            sudo sh "Download In Backround.sh"
+            sudo sh downloader.sh
+        ;;
+        3)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nes.sh
+            sudo bash ./nes.sh
+            sudo rm -r nes.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        4)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/snes.sh
+            sudo bash ./snes.sh
+            sudo rm -r snes.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        5)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/gb.sh
+            sudo bash ./gb.sh
+            sudo rm -r gb.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        6)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/gbc.sh
+            sudo bash ./gbc.sh
+            sudo rm -r gbc.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        7)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/gba.sh
+            sudo bash ./gba.sh
+            sudo rm -r gba.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        8)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nes.sh
+            sudo bash ./n64.sh
+            sudo rm -r nes.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        9)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/nds.sh
+            sudo bash ./nds.sh
+            sudo rm -r nds.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        10)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/virtualboy.sh
+            sudo bash ./virtualboy.sh
+            sudo rm -r virtualboy.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        11)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/megadrive.sh
+            sudo bash ./megadrive.sh
+            sudo rm -r megadrive.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        12)
+
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/saturn.sh
+            sudo bash ./saturn.sh
+            sudo rm -r saturn.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        13)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/dreamcast.sh
+            sudo bash ./dreamcast.sh
+            sudo rm -r dreamcast.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+        14)
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/psx.sh
+            sudo bash ./psx.sh
+            sudo rm -r psx.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;  
+        15) 
+            sudo wget https://raw.githubusercontent.com/oldangrysheep/raspidownloads/main/gui/gameselection/psp.sh
+            sudo bash ./psp.sh
+            sudo rm -r psp.sh
+            cd /usr/bin/RomDownloader/Temp
+            sudo sh gamelist.sh
+        ;;
+    esac
 }
 
 Media () { 
